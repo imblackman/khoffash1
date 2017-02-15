d69e5a6b4b0a67ee13dc56037e9d5d9fe731e848
imblackman <blackmanshar@gmail.com>
2017-02-15 17:06:38 +0330

    Create groupmanagerfa.lua

---
+++ plugins/groupmanagerfa.lua
@@ -0,0 +1,49 @@
+local function modadd(msg)
+local hash = "gp_lang:"..msg.chat_id_
+local lang = redis:get(hash)
+    if not is_admin(msg) then
+   if not lang then
+        return '_You are not bot admin_'
+else
+     return 'شما مدیر ربات نمیباشید'
+    end
+end
+    local data = load_data(_config.moderation.data)
+  if data[tostring(msg.chat_id_)] then
+if not lang then
+   return '_Group is already added_'
+else
+return 'گروه در لیست گروه های مدیریتی ربات هم اکنون موجود است'
+  end
+end
+        -- create data array in moderation.json
+      data[tostring(msg.chat_id_)] = {
+              owners = {},
+      mods ={},
+      banned ={},
+      is_silent_users ={},
+      filterlist ={},
+      settings = {
+          lock_link = 'yes',
+          lock_tag = 'yes',
+		  lock_fosh = 'yes',
+          lock_spam = 'no',
+          lock_webpage = 'yes',
+		  lock_arabic = 'no',
+          lock_markdown = 'yes',
+          flood = 'yes',
+          lock_bots = 'yes',
+          welcome = 'yes'
+          },
+   mutes = {
+                  mute_forward = 'no',
+                  mute_audio = 'no',
+                  mute_video = 'no',
+                  mute_contact = 'no',
+                  mute_text = 'no',
+                  mute_photos = 'no',
+                  mute_gif = 'no',
+                  mute_location = 'no',
+                  mute_document = 'no',
+                  mute_sticker = 'no',
+                  mute_voice = 'no',
